de3e95bdcd4970de36a82e10ee9e593de9899f35
hyozkim <sa833591@gmail.com>
2021-10-03 18:31:02 +0900

    feat: game start
    - 상대방(컴퓨터) 서로 다른 숫자 3개 제시

---
+++ src/main/java/baseball/service/BaseBallGame.java
@@ -0,0 +1,13 @@
+package baseball.service;
+
+import java.util.List;
+
+public class BaseBallGame {
+
+
+    public static void start(Pitcher pitcher) {
+        List<Integer> ball = pitcher.pitch(); // 상대방(컴퓨터) 투구. (서로 다른 수 3개 제시)
+
+
+    }
+}
